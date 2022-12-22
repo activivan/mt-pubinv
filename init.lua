db9e30732fef961f1bb48ab4918d0e0de580b87d
activivan <activivan.studios@gmail.com>
2022-12-22 01:40:01 +0100

    Add inventory integrations

--- init.lua
+++ init.lua
@@ -8,6 +8,10 @@ local conf = minetest.settings
 local cols = tonumber(conf:get("pubinv_cols")) or 9
 local rows = tonumber(conf:get("pubinv_rows")) or 3
 
+
+-- ---------------------------
+-- Detached inventory handling
+
 local function save(inv_list)
 	local data = {}
 
@@ -41,14 +45,18 @@ end
 load()
 
 
+-- ---------------------------
+-- API
+
 local pubinv = {} -- Remove "local" to make API globally accessible
 
-pubinv.open = function(name)
+pubinv.formspec = function(name)
 	local inv_size = (minetest.get_inventory({ type="player", name=name })):get_size("main")
 	local inv_rows = math.ceil(inv_size / cols)
 
-	minetest.show_formspec(name, "pubinv:pi",
-			"size["..cols..","..(rows + 1.25 + inv_rows)..";]"..
+	local size = "size["..cols..","..(rows + 1.25 + inv_rows)..";]"
+
+	return (size..
 			"label[0,0;"..S("Public inventory").."]"..
 			"list[detached:pi;main;0,0.5;"..cols..","..rows..";]"..
 			(mcl_formspec and mcl_formspec.get_itemslot_bg(0, 0.5, cols, rows) or "")..
@@ -58,14 +66,44 @@ pubinv.open = function(name)
 				mcl_formspec.get_itemslot_bg(0, (rows + 1.25), ((inv_size > cols) and cols or inv_size), ((inv_size > cols) and (inv_rows - 1) or 1))..
 				((inv_size > cols) and mcl_formspec.get_itemslot_bg(0, (rows + inv_rows + 0.25), (inv_size - (cols * (inv_rows - 1))), 1) or "")
 			or "")..
-			"listring[]")
+			"listring[]"), size
+end
+
+pubinv.open = function(name)
+	minetest.show_formspec(name, "pubinv:pi", pubinv.formspec(name))
 	return true, ""
 end
 
+
+-- ---------------------------
+-- Integrations
+
 minetest.register_chatcommand("pi", {
 	params = "",
-	description = "Opens the public inventory",
+	description = S("Opens the public inventory"),
 	func = function(name)
 		pubinv.open(name)
 	end,
 })
+
+if unified_inventory then
+	unified_inventory.register_button("pubinv", {
+		type = "image",
+		image = "ui_icon_pubinv.png",
+		tooltip = S("Public inventory"),
+		hide_lite = false,
+		action = function(player)
+			pubinv.open(player:get_player_name())
+		end
+	})
+end
+
+if sfinv then
+	sfinv.register_page("pubinv", {
+		title = S("Public inventory"),
+		get = function(self, player, context)
+			local formspec, size = pubinv.formspec(player:get_player_name())
+			return sfinv.make_formspec(player, context, formspec, false, size)
+		end
+	})
+end
